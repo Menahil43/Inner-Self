6782777ddf2ef3b7eaf22a7e6bbd0d71915aafa8
Rida-thedeveloper <ridathedeveloper@gmail.com>
2024-12-02 21:30:15 +0500

    Add files via upload

---
+++ Documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"quiz.app","l":"EQTest"},{"p":"quiz.app","l":"Score"},{"p":"quiz.app","l":"SessionStarter"}];updateSearchResults();
